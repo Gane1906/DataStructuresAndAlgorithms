3f9c2df840afa6825a46a838d6d43e23184d8825
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-24 11:42:42 +0530

    [Ganesh] Add:Data structures and algorithms file is added

---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DataStructuresAndAlgorithms
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Data structures and algorithms!");
+        }
+    }
+}
