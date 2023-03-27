86962858a63e92f8ae5d105ee414a360afb28490
Gane1906 <109649964+Gane1906@users.noreply.github.com>
2023-03-27 20:47:42 +0530

    Update MergeSort.cs

--- DataStructuresAndAlgorithms/MergeSort.cs
+++ DataStructuresAndAlgorithms/MergeSort.cs
@@ -11,32 +11,15 @@ namespace DataStructuresAndAlgorithms
     {
         public void Merge(int[] arr, int l, int m, int r)
         {
-            // Find sizes of two
-            // subarrays to be merged
             int n1 = m - l + 1;
             int n2 = r - m;
-
-            // Create temp arrays
             int[] L = new int[n1];
             int[] R = new int[n2];
-            int i, j;
-
-            // Copy data to temp arrays
-            for (i = 0; i < n1; ++i)
+            for (int i = 0; i < n1; ++i)
                 L[i] = arr[l + i];
-            for (j = 0; j < n2; ++j)
+            for (int j = 0; j < n2; ++j)
                 R[j] = arr[m + 1 + j];
-
-            // Merge the temp arrays
-
-            // Initial indexes of first
-            // and second subarrays
-            i = 0;
-            j = 0;
-
-            // Initial index of merged
-            // subarray array
-            int k = l;
+            int i=0,j=0, k = l;
             while (i < n1 && j < n2)
             {
                 if (L[i] <= R[j])
@@ -51,18 +34,12 @@ namespace DataStructuresAndAlgorithms
                 }
                 k++;
             }
-
-            // Copy remaining elements
-            // of L[] if any
             while (i < n1)
             {
                 arr[k] = L[i];
                 i++;
                 k++;
             }
-
-            // Copy remaining elements
-            // of R[] if any
             while (j < n2)
             {
                 arr[k] = R[j];
