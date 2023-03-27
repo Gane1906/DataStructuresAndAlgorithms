0780d05e25c8cb6819d8b2748602a77542b8fc38
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-27 12:23:32 +0530

    [Ganesh] Add:Anagram check is added

---
+++ DataStructuresAndAlgorithms/AnagramCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class AnagramCheck
+    {
+        public void CheckAnagram()
+        {
+            Console.WriteLine("Enter first word");
+            String firstInput = Console.ReadLine();
+            Console.WriteLine("Enter second word");
+            string secondInput = Console.ReadLine();
+            if (firstInput.Length != secondInput.Length)
+            {
+                Console.WriteLine("Thw eords are not anagram");
+            }
+            else
+            {
+                char[] result1 = firstInput.ToCharArray();
+                Array.Sort(result1);
+                String word1 = result1.ToString();
+                char[] result2 = secondInput.ToCharArray();
+                Array.Sort(result2);
+                String word2 = result2.ToString();
+                if (word1.Equals(word2))
+                {
+                    Console.WriteLine("The words are anagram");
+                }
+                else
+                {
+                    Console.WriteLine("The words are not anagram");
+                }
+            }
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -9,12 +9,13 @@ namespace DataStructuresAndAlgorithms
             searchWord.ReadFile(@"E:\Users\Ganesh\Documents\BL Excercise\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\File.txt");
             InsertionSort insertionSort = new InsertionSort();
             insertionSort.ReadFile(@"E:\Users\Ganesh\Documents\BL Excercise\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\File.txt");
-            
             BubbleSort bubbleSort = new BubbleSort();
             bubbleSort.ReadFile(@"E:\Users\Ganesh\Documents\BL Excercise\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\IntegerFile.txt");
-            */
             MergeSort mergeSort = new MergeSort();
             mergeSort.ReadFie(@"E:\Users\Ganesh\Documents\BL Excercise\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\IntegerFile.txt");
+            */
+            AnagramCheck anagram = new AnagramCheck();
+            anagram.CheckAnagram();
         }
     }
 }
